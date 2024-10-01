c2f94756d26b1c52f9a8759b77430b48091f1cb9
vamshichi <chvamshi03@gmail.com>
2024-10-02 00:11:33 +0530

    added news-section and seo  image

--- app/termsofservice/page.tsx
+++ app/termsofservice/page.tsx
@@ -1,4 +1,9 @@
 import TermsOfService from "@/app/components/policy/TermsOfService";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Terms Of Service',
+};
 
 const PrivacyPage: React.FC = () => {
   return (
